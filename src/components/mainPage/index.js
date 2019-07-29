10b58d016eb7832fe4e1f9e482050363d6260521
SADmammoth <lo.ma200018@gmail.com>
2019-07-29 23:57:51 +0300

    feat: mainpage main block
    styles and positioning

--- src/components/mainPage/index.js
+++ src/components/mainPage/index.js
@@ -1,3 +1,5 @@
 import mainPage from './mainPage';
+import './reset.css'
+import './style.css'
 
 export default mainPage;
--- src/components/mainPage/mainPage.js
+++ src/components/mainPage/mainPage.js
@@ -1,19 +1,32 @@
-import { Link, graphql } from "gatsby"
+import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
 import React from "react"
+import { Grid, Container, Card } from "@material-ui/core";
 
 import RenderRichText from '../RenderRichText'
 import RenderImage from '../RenderImage'
 
 const MainPage = ({ data }) => {
   const MainPage = data.english.nodes[0]
-  console.log(MainPage.developerTeamDesc)
   return (<React.Fragment>
-    <h1>{MainPage.title}</h1>
-    <div>
-      {RenderImage(MainPage.mainPicture.resolutions)}
-      {RenderRichText(MainPage.desc.json)}</div>
+    <Helmet>
+      <title>{MainPage.title}</title>
+      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+    </Helmet>
+    <Grid container
+      direction="column"
+      justify="center"
+      alignItems="flex-start"
+      style={{ width: '100%', height: '65vh', background: `center / cover url(${MainPage.mainPicture.resolutions.src})` }}>
+      <Container maxWidth='md'>
+        <h1>{MainPage.title}</h1>
+        <div style={{ width: '90%', marginLeft: '10%' }}>{RenderRichText(MainPage.desc.json)}</div>
+      </Container>
+    </Grid>
+    {/* 
     <div>{RenderRichText(MainPage.developerTeamDesc.json)}</div>
-    <ul>{MainPage.githubLinks.map(link => <li><a href={link}>{link}</a></li>)}</ul>
+    <ul>{MainPage.githubLinks.map(link => <li><a href={link}>{link}</a></li>)}</ul> */}
     <Link to="/photographers-list/">Go to photographers list page</Link>
   </React.Fragment>)
 }
