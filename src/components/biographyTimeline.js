873f02e585def6b0147f581f7d006bf84e4670cb
Natalia Komarova <nataliko.ru@mail.ru>
2019-07-29 14:35:59 +0300

    refactor: apply prettier

--- src/components/biographyTimeline.js
+++ src/components/biographyTimeline.js
@@ -1,38 +1,29 @@
-import React from "react";
-import { Timeline, TimelineItem }  from 'vertical-timeline-component-for-react';
+import React from "react"
+import { Timeline, TimelineItem } from "vertical-timeline-component-for-react"
 
-const BiographyTimeline = ({biographyList}) => {
-  console.log(biographyList);
-  const colorsOfDates = ['#6C8CD5', '#FFD073'];
-  
-  // const dateText = dateText={date}
+const BiographyTimeline = ({ biographyList }) => {
+  console.log(biographyList)
+  const colorsOfDates = ["#6C8CD5", "#FFD073"]
+
   const timelineItems = biographyList.map((bioItem, index) => {
-    const hyphenIndex = bioItem.lastIndexOf('-');
-    const firstLetter = bioItem.search(/[a-zа-я]/i);
+    const firstLetter = bioItem.search(/[a-zа-я]/i)
+    const dateText = bioItem.slice(0, firstLetter - 1)
+    const bioText = bioItem.slice(firstLetter)
+    
+    const colorOfDate = colorsOfDates[index % colorsOfDates.length]
 
-    const dateText = bioItem.slice(0, firstLetter-1);
-    const bioText = bioItem.slice(firstLetter);
-    const colorOfDate = colorsOfDates[index % colorsOfDates.length];
-
-    console.log(colorOfDate);
-    console.log(bioItem.split('-'));
     return (
       <TimelineItem
         dateText={dateText}
         key={index.toString()}
-        dateInnerStyle={ {background: colorOfDate} }
+        dateInnerStyle={{ background: colorOfDate }}
       >
         {bioText}
       </TimelineItem>
     )
   })
 
-  return (
-    <Timeline lineColor={'#ddd'}>
-      {timelineItems}
-    </Timeline>
-  );
-};
+  return <Timeline lineColor={"#ddd"}>{timelineItems}</Timeline>
+}
 
-export default BiographyTimeline;
-
+export default BiographyTimeline
--- src/components/photographer.js
+++ src/components/photographer.js
@@ -1,29 +1,30 @@
-import { Link } from "gatsby";
-import React from "react";
-import BiographyTimeline from './biographyTimeline';
+import { Link } from "gatsby"
+import React from "react"
+import BiographyTimeline from "./biographyTimeline"
 
-const Photographer = ({data}) => {
-  const authorInfo = data.contentfulAuthors;
-  const { biography } = authorInfo.biography;
-  const { biographyList } = authorInfo;
+const Photographer = ({ data }) => {
+  const authorInfo = data.contentfulAuthors
+  const { biography } = authorInfo.biography
+  const { biographyList } = authorInfo
 
   return (
     <div className="author-page">
       <BiographyTimeline biographyList={biographyList} />
     </div>
-  );
-};
+  )
+}
 
-export default Photographer;
+export default Photographer
 
 export const PhotographerQuery = graphql`
-query($slug: String!) {
-  contentfulAuthors(slug: {eq: $slug}) {
-    biography {
-      biography
+  query($slug: String!) {
+    contentfulAuthors(slug: { eq: $slug }) {
+      biography {
+        biography
+      }
+      biographyList
+      initials
+      videolink
     }
-    biographyList
-    initials
-    videolink
   }
-}`;
+`
