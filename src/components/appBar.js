f8e509d1cb5f95e6c14cf98f7e3c687d6a82556b
Aleksey <kedraja@gmail.com>
2019-07-31 12:23:11 +0300

    commit

--- src/components/appBar.js
+++ src/components/appBar.js
@@ -147,7 +147,7 @@ export default function PrimarySearchAppBar() {
 
   return (
     <div className={classes.grow}>
-      <AppBar position="static">
+      <AppBar position="static" backgroundColor="red">
         <Toolbar>
           <IconButton
             edge="start"
