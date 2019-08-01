061bf1f4a8739f3ef0035932d6bdd1ad4e7e22e5
DzmitryKas <Kasperovich_dima@mail.ru>
2019-08-01 21:21:04 +0300

    feat: add styles for video and modal window

--- src/components/videoInformation.js
+++ src/components/videoInformation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ModalVideo from 'react-modal-video';
+import './css/modal-video.min.css';
 
 export default class VideoInformation extends React.Component {
 
@@ -17,11 +18,18 @@ export default class VideoInformation extends React.Component {
 
   render () {
     const linkImage = `https://img.youtube.com/vi/${this.props.videolink}/hqdefault.jpg` 
+    const StyleButtonVideo = {
+      display: "block",
+      margin: "30px auto",
+      maxWidth: "800px",
+      height: "550px",  
+      cursor: "pointer" 
+    }
     
     return (
-      <div>
+      <div style={StyleButtonVideo}>
         <ModalVideo channel='youtube' isOpen={this.state.isOpen} videoId={this.props.videolink} onClose={() => this.setState({isOpen: false})} />
-        <img  src={linkImage}  onClick={this.openModal}/>          
+        <img style={{maxWidth: "100%", height: "550px"}}  src={linkImage}  onClick={this.openModal}/>          
       </div>
     )
   }
