cadb9516dd67019e4e61cd706cbd567bee343149
SADmammoth <lo.ma200018@gmail.com>
2019-08-02 15:59:42 +0300

    feat: minor improvements
    Some variables naming; link in blank tab; link to github now applied to both badge and avatar; different style changes

--- src/components/mainPage/mainPage.js
+++ src/components/mainPage/mainPage.js
@@ -73,22 +73,24 @@ const MainPage = ({ data }) => {
 
   let classes = useStyles()
 
-  const MainPage = data.english.nodes[0]
+  const MainpageData = data.russian.nodes[0]
 
-  const GithubIcon = RenderImage(classes.containerWidePic, MainPage.githubIcon.resolutions)
+  const GithubIcon = RenderImage(classes.containerWidePic, MainpageData.githubIcon.resolutions)
 
   /*Function processing data for each team participant*/
   function devTeamParts() {
     let arr = [];
-    for (let i = 0; i < 7; i++) {
+    for (let i = 0; i < MainpageData.devPartNames.length; i++) {
       arr.push(
         <Grid item key={shortid.generate()} component='li' zeroMinWidth style={{ width: '280px', minWidth: '22%', maxWidth: '90%', marginTop: '4%', listStyleType: 'none' }} >
-          <Badge badgeContent={<a className={classes.smallBadge} title={MainPage.githubLinks[i]} href={MainPage.githubLinks[i]}>{GithubIcon}</a>}>
-            <Avatar className={classes.bigAvatar} title={MainPage.teamPhotos[i].title} src={MainPage.teamPhotos[i].resolutions.src} alt={MainPage.teamPhotos[i].title} />
-          </Badge>
+          <a title={MainpageData.githubLinks[i]} href={MainpageData.githubLinks[i]} target='_blank' rel="noopener noreferrer">
+            <Badge badgeContent={<div className={classes.smallBadge}>{GithubIcon}</div>}>
+              <Avatar className={classes.bigAvatar} title={MainpageData.teamPhotos[i].title} src={MainpageData.teamPhotos[i].resolutions.src} alt={MainpageData.teamPhotos[i].title} />
+            </Badge>
+          </a>
           <Container maxWidth={false} style={{ textAlign: 'center', padding: '3% 0 0' }}>
-            <Typography variant='h5' component='h3' color='primary'>{MainPage.devPartNames[i]}</Typography>
-            <Typography variant='body1' >{MainPage.devPartDesc[i]}</Typography>
+            <Typography variant='h5' component='h3' color='primary'>{MainpageData.devPartNames[i]}</Typography>
+            <Typography variant='body1' >{MainpageData.devPartDesc[i]}</Typography>
           </Container>
         </Grid>)
     }
@@ -98,22 +100,22 @@ const MainPage = ({ data }) => {
   return (
     <div className={classes.root}>
       <Helmet>
-        <title>{MainPage.title}</title>
+        <title>{MainpageData.title}</title>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       </Helmet>
       <Paper>
-        <Container maxWidth={false} style={{ padding: '11% 0', background: `center / cover url(${MainPage.mainPicture.resolutions.src})` }}>
+        <Container maxWidth={false} style={{ padding: '11% 0', background: `center / cover url(${MainpageData.mainPicture.resolutions.src})` }}>
           <Container maxWidth='lg'>
-            <Typography variant='h1' color='primary'> <Box fontWeight="fontWeightMedium" component='strong'>{MainPage.title}</Box></Typography>
-            <Container style={{ marginTop: '30px' }}>
-              {RenderRichText(MainPage.desc.json)}
+            <Typography variant='h1' color='primary'> <Box fontWeight="fontWeightMedium" component='strong'>{MainpageData.title}</Box></Typography>
+            <Container style={{ marginTop: '30px', marginLeft: '-2%', maxWidth: '98vw' }}>
+              {RenderRichText(MainpageData.desc.json)}
             </Container>
           </Container>
         </Container>
       </Paper>
       <Container maxWidth={false} style={{ position: 'relative', zIndex: '-1', paddingTop: '5%', paddingBottom: '7%', background: '#ECF5FF' }}>
-        <Container maxWidth='lg' style={{ paddingLeft: '10%' }}>
-          {RenderRichText(MainPage.authorOfTheDayDesc.json)}
+        <Container maxWidth='lg' style={{ paddingLeft: '20%' }}>
+          {RenderRichText(MainpageData.authorOfTheDayDesc.json)}
         </Container>
       </Container>
       <Container maxWidth='lg' style={{ marginTop: '-5%' }}>
@@ -122,15 +124,15 @@ const MainPage = ({ data }) => {
           justify="center"
           alignItems="center">
           <PhCard
-            key={MainPage.authorOfTheDay.initials}
-            phr={MainPage.authorOfTheDay}
-            slug={MainPage.authorOfTheDay.slug}>
+            key={MainpageData.authorOfTheDay.initials}
+            phr={MainpageData.authorOfTheDay}
+            slug={MainpageData.authorOfTheDay.slug}>
           </PhCard>
         </Grid>
       </Container>
-      <Button href='/photographers-list/' className={classes.rightAlignedWideBtn} style={{ margin: '2% 0' }}>{(function () { switch (MainPage.node_locale) { case ('ru'): return 'Показать всех'; case ('en-US'): return 'See all'; default: throw Error('Indefined locale') } }())}</Button>
+      <Button href='/photographers-list/' className={classes.rightAlignedWideBtn} style={{ margin: '2% 0' }}>{(function () { switch (MainpageData.node_locale) { case ('ru'): return 'Показать всех'; case ('en-US'): return 'See all'; default: throw Error('Indefined locale') } }())}</Button>
       <Container maxWidth='lg' style={{ padding: '2% 10%' }}>
-        {RenderRichText(MainPage.developerTeamDesc.json)}
+        {RenderRichText(MainpageData.developerTeamDesc.json)}
       </Container>
       <Container maxWidth={false} className={classes.fullWide} style={{ background: 'linear-gradient(to right, #ECF5FF 50%, #ECF5FF00 50%)' }}>
         <Container maxWidth='lg' style={{ padding: '3% 5%', backgroundColor: '#ECF5FF' }}>
