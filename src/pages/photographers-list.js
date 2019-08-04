fb204beed35a40d5dbf671e8672b2aef210197e9
Alena Vasilchenko <vasilchenko.ale@gmail.com>
2019-08-04 22:44:39 +0300

    feat: add simple search

--- src/pages/photographers-list.js
+++ src/pages/photographers-list.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/layout";
 import { graphql } from 'gatsby';
 import { Typography, Container } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
 import PhCard from '../components/phCard';
+import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles((theme) => ({
   mainTitle: {
@@ -18,6 +20,47 @@ const useStyles = makeStyles((theme) => ({
     flexWrap: 'wrap',
     justifyContent: 'space-around',
   },
+  search: {
+    marginBottom: 30,
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    [theme.breakpoints.up('md')]: {
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+    },
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('xs')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      }
+    }
+  }
 }));
 
 const photographersList = ({ data }) => {
@@ -36,7 +79,21 @@ const photographersList = ({ data }) => {
       >
       </PhCard >
     );
-  })
+  });
+
+  let [filteredList, setFilteredList] = useState(authorsList);
+
+
+  const handleSearch = function (event) {
+    const searchQuery = event.target.value.toLowerCase();
+
+    const fList = authorsList.filter((function (el) {
+      const searchValue = el.props.phr.initials.toLowerCase();
+      return searchValue.indexOf(searchQuery) !== -1;
+    }));
+
+    setFilteredList(fList);
+  }
 
   return (
     <Layout>
@@ -44,13 +101,30 @@ const photographersList = ({ data }) => {
         <Typography className={classes.mainTitle} variant='h4' gutterBottom>
           Photographers of Belarus
         </Typography>
+
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder="Search…"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            inputProps={{ 'aria-label': 'search' }}
+            onChange={handleSearch}
+          />
+        </div>
+
         <Container className={classes.authorsList}>
-          {authorsList}
+          {filteredList}
         </Container>
       </Container>
     </Layout>
   );
 }
+
 
 export default photographersList;
 
