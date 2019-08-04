93c8d675e1908938948d72dfb553be55dc91dfbc
Aleksey <kedraja@gmail.com>
2019-08-04 18:51:22 +0300

    add location h

--- src/components/yandex-map.js
+++ src/components/yandex-map.js
@@ -1,18 +1,19 @@
 import React from "react";
-
+import { Typography } from '@material-ui/core';
 function YandexMap(props) {
   const StyleWrapperMap = {
     display: "block",
-    margin: "100px auto",
+    margin: "40px auto",
     maxWidth: "800px",
     width: "100%",
     height: "550px",
   }
   
   return (
-    
+    <div>
+    <div><Typography gutterBottom variant="h4" component="h3" align="center">Location</Typography></div>
     <div style={StyleWrapperMap} dangerouslySetInnerHTML={{__html: props.mapCode}}>     
-    </div>
+    </div></div>
   )
 }
 
