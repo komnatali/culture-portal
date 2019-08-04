6c0ae5f0d9cf9da9d251908ef31f65027bc1a2cf
Alena Vasilchenko <vasilchenko.ale@gmail.com>
2019-08-04 11:58:21 +0300

    style: remove console.log

--- src/components/yandex-map.js
+++ src/components/yandex-map.js
@@ -8,7 +8,7 @@ function YandexMap(props) {
     width: "100%",
     height: "550px",
   }
-  console.log('props', props.mapCode)
+  
   return (
     <div style={StyleWrapperMap} dangerouslySetInnerHTML={{__html: props.mapCode}}>     
     </div>
